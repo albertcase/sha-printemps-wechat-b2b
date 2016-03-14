c92cd4fdd99adf0bae3bfda2043ae11b0893eee7
Dirc Wang <dirk.wang@samesamechina.com>
2016-03-14 22:55:50 +0800

    update weixin2

--- protected/models/Weixin.php
+++ protected/models/Weixin.php
@@ -13,7 +13,7 @@ class Weixin{
 
 	public function __construct()
 	{
-// 		$this->_memcache = new memcaches();
+		$this->_memcache = new memcaches();
 		if( $this->_db===null)
 			$this->_db = Yii::app()->db;
 	}
