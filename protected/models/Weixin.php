4ab76fbcd55c75bbb93a2611eeffec34d6b5a44e
Dirc Wang <dirk.wang@samesamechina.com>
2016-03-14 15:29:54 +0800

    add transfer cunstom32

--- protected/models/Weixin.php
+++ protected/models/Weixin.php
@@ -69,7 +69,9 @@ class Weixin{
 	                			$data[] = array('title'=>$rs[$i]['title'],'description'=>$rs[$i]['description'],'picUrl'=>$rs[$i]['url']);
 	                		}
 	                		return $this->sendMsgForNews($fromUsername, $toUsername, $time, $data);
-	                	}else{
+	                	}else if($rs[0]['msgtype'] == 'transfer_customer_service'){
+											return $this->transferService($fromUsername, $toUsername ,trim($rs[0]['content']));//切换服务
+										}else{
 	                		return $this->sendService($fromUsername, $toUsername);
 	                	}
                 	}
