01824cfd2f7a345d6a455610f2b584db275466a7
Demon Zhang <demon.zhang@samesamechina.com>
2016-03-17 17:11:10 +0800

    no message

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -261,8 +261,7 @@ class ApiController extends Controller
 			$line = str_replace(' ','',$line);
 			$line = str_replace('\r','',$line);
 			$line = str_replace('\r\n','',$line);
-			$regex = "/\/|\~|\!|\@|\#|\\$|\%|\^|\&|\*|\(|\)|\_|\+|\{|\}|\:|\<|\>|\?|\[|\]|\.|\/|\;|\'|\`|\-|\=|\\\|\|/";
-    	    $line = preg_replace($regex,"",$line);
+			$line = preg_replace("/[//x00-//x08//x0b-//x0c//x0e-//x1f]/",'',$line);  
 			$lineAry = explode(",", $line);
 			if(count($lineAry)!=4){
 				continue;
