ad0a2346a40605c68fcc09fb4a672cdf76314555
Dirc Wang <dirk.wang@samesamechina.com>
2016-05-08 17:55:17 +0800

    add same function 6

--- printempschat/index.php
+++ printempschat/index.php
@@ -1,4 +1,5 @@
 <?php
+ini_set("display_errors","on");
 require_once dirname(__FILE__).'/wechat.php';
 $access_token=file_get_contents(dirname(__FILE__)."/../upload/access_token.txt");
 print $access_token."\n";
