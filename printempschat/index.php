33a0359503a90f57b1fb88998752f4359c1359f8
Dirc Wang <dirk.wang@samesamechina.com>
2016-05-08 17:52:19 +0800

    add same function 5

--- printempschat/index.php
+++ printempschat/index.php
@@ -1,3 +1,7 @@
 <?php
+require_once dirname(__FILE__).'/wechat.php';
 $access_token=file_get_contents(dirname(__FILE__)."/../upload/access_token.txt");
-print $access_token;
+print $access_token."\n";
+$wechat = new wechat();
+print_r($wechat->backaccesstoken());
+print_r($wechat->deacccesstoken());
--- printempschat/wechat.php
+++ printempschat/wechat.php
@@ -3,6 +3,8 @@ class wechat{
   private $_TOKEN = 'printempsb2b';
   private $_appid = 'wx5724db07982c3896';
   private $_secret = 'd9e9ae55f59bb02fa1b71146520cda03';
+  private $key = '29FB77CB8E94B358';
+  private $iv = '6E4CAB2EAAF32E90';
 
   public function getAccessToken()
   {
@@ -28,9 +30,40 @@ class wechat{
         fclose($fp);
         return $rs['access_token'];
       }else{
-        return $rs['errcode'];
+        return false;
       }
     }
     return $access_token;
+  }
+
+  public function aes128_cbc_encrypt($key, $data, $iv) {
+    if(16 !== strlen($key)) $key = hash('MD5', $key, true);
+    if(16 !== strlen($iv)) $iv = hash('MD5', $iv, true);
+    $padding = 16 - (strlen($data) % 16);
+    $data .= str_repeat(chr($padding), $padding);
+    return mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $data, MCRYPT_MODE_CBC, $iv);
+  }
+
+  public function aes128_cbc_decrypt($key, $data, $iv) {
+    if(16 !== strlen($key)) $key = hash('MD5', $key, true);
+    if(16 !== strlen($iv)) $iv = hash('MD5', $iv, true);
+    $data = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $data, MCRYPT_MODE_CBC, $iv);
+    $padding = ord($data[strlen($data) - 1]);
+    return substr($data, 0, -$padding);
+  }
+
+  public function backaccesstoken(){
+    if($AccessToken = $this->getAccessToken()){
+      return array(
+        'code' => '10',
+        'access_token' => $this->aes128_cbc_encrypt($this->key, $AccessToken, $this->iv),
+    );
+    }
+    return array('code' => '9');
+  }
+
+  public function deacccesstoken(){
+    $token = $this->backaccesstoken();
+    return aes128_cbc_decrypt($this->key, $token['access_token'], $this->iv);
   }
 }
