84c22696ebe3ad731e0693c289764e4bbb794744
Dirc Wang <dirk.wang@samesamechina.com>
2016-06-16 17:05:54 +0800

    add upload excel books10

--- protected/views/site/list.php
+++ protected/views/site/list.php
@@ -24,7 +24,7 @@
             <!-- <option value="bak">Region</option> -->
             <option value="openidd">Login</option>
           </select>
-          <i class="fa fa-chevron-down" style="color:#ddd;margin-left:-10px"></i>
+          <!-- <i class="fa fa-chevron-down" style="color:#ddd;margin-left:-10px"></i> -->
           <i class="fa fa-plus-square"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           SHOW:
           <select id="everypage">
@@ -33,7 +33,7 @@
             <option value="40">40</option>
             <option value="50">50</option>
           </select>
-          <i class="fa fa-chevron-down" style="color:#ddd"></i>
+          <!-- <i class="fa fa-chevron-down" style="color:#ddd"></i> -->
           <!-- <button class="btn-blue" id="searchbt" style="margin-left:40px;">Search</button> -->
           <span id="searchbt" style="margin-left:40px;cursor:pointer">SEARCH</span>
           &nbsp;&nbsp;&nbsp;&nbsp;
